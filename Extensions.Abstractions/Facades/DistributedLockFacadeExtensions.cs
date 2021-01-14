5daefd6969395f254c8347db1c44d05de69a0ee0
Dawid Szmidka <dawid.szmidka@gmail.com>
2021-01-14 01:21:02 +0100

    Update readme

--- Extensions.Abstractions/Facades/DistributedLockFacadeExtensions.cs
+++ Extensions.Abstractions/Facades/DistributedLockFacadeExtensions.cs
@@ -27,6 +27,50 @@ namespace DistributedLocking.Extensions.Abstractions.Facades
                     cancellationToken)
                 .ConfigureAwait(false);
         }
+        
+        public static async Task<IDistributedLock> AcquireAsync(
+            this IDistributedLockFacade facade,
+            DistributedLockIdentifier identifier,
+            DistributedLockTimeToLive timeToLive,
+            TimeSpan timeout,
+            TimeSpan delayBetweenRetries,
+            CancellationToken cancellationToken)
+        {
+            if (facade == null)
+            {
+                throw new ArgumentNullException(nameof(facade));
+            }
+
+            var policy = new TimeoutRetryPolicy(timeout, delayBetweenRetries);
+            return await facade.AcquireAsync(
+                    identifier,
+                    timeToLive,
+                    policy,
+                    cancellationToken)
+                .ConfigureAwait(false);
+        }
+        
+        public static async Task<IDistributedLock> AcquireAsync(
+            this IDistributedLockFacade facade,
+            DistributedLockIdentifier identifier,
+            DistributedLockTimeToLive timeToLive,
+            TimeSpan timeout,
+            Func<RetryExecutionMetadata, TimeSpan> delayFunc,
+            CancellationToken cancellationToken)
+        {
+            if (facade == null)
+            {
+                throw new ArgumentNullException(nameof(facade));
+            }
+
+            var policy = new TimeoutRetryPolicy(timeout, delayFunc);
+            return await facade.AcquireAsync(
+                    identifier,
+                    timeToLive,
+                    policy,
+                    cancellationToken)
+                .ConfigureAwait(false);
+        }
 
         public static async Task ExtendAsync(
             this IDistributedLockFacade facade,
@@ -43,6 +87,50 @@ namespace DistributedLocking.Extensions.Abstractions.Facades
                     distributedLock,
                     timeToLive,
                     NoRetriesPolicy.Instance,
+                    cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        public static async Task ExtendAsync(
+            this IDistributedLockFacade facade,
+            IDistributedLock distributedLock,
+            DistributedLockTimeToLive timeToLive,
+            TimeSpan timeout,
+            TimeSpan delayBetweenRetries,
+            CancellationToken cancellationToken)
+        {
+            if (facade == null)
+            {
+                throw new ArgumentNullException(nameof(facade));
+            }
+
+            var policy = new TimeoutRetryPolicy(timeout, delayBetweenRetries);
+            await facade.ExtendAsync(
+                    distributedLock,
+                    timeToLive,
+                    policy,
+                    cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        public static async Task ExtendAsync(
+            this IDistributedLockFacade facade,
+            IDistributedLock distributedLock,
+            DistributedLockTimeToLive timeToLive,
+            TimeSpan timeout,
+            Func<RetryExecutionMetadata, TimeSpan> delayFunc,
+            CancellationToken cancellationToken)
+        {
+            if (facade == null)
+            {
+                throw new ArgumentNullException(nameof(facade));
+            }
+
+            var policy = new TimeoutRetryPolicy(timeout, delayFunc);
+            await facade.ExtendAsync(
+                    distributedLock,
+                    timeToLive,
+                    policy,
                     cancellationToken)
                 .ConfigureAwait(false);
         }
@@ -64,6 +152,46 @@ namespace DistributedLocking.Extensions.Abstractions.Facades
                 .ConfigureAwait(false);
         }
 
+        public static async Task ReleaseAsync(
+            this IDistributedLockFacade facade,
+            IDistributedLock distributedLock,
+            TimeSpan timeout,
+            TimeSpan delayBetweenRetries,
+            CancellationToken cancellationToken)
+        {
+            if (facade == null)
+            {
+                throw new ArgumentNullException(nameof(facade));
+            }
+
+            var policy = new TimeoutRetryPolicy(timeout, delayBetweenRetries);
+            await facade.ReleaseAsync(
+                    distributedLock,
+                    policy,
+                    cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        public static async Task ReleaseAsync(
+            this IDistributedLockFacade facade,
+            IDistributedLock distributedLock,
+            TimeSpan timeout,
+            Func<RetryExecutionMetadata, TimeSpan> delayFunc,
+            CancellationToken cancellationToken)
+        {
+            if (facade == null)
+            {
+                throw new ArgumentNullException(nameof(facade));
+            }
+
+            var policy = new TimeoutRetryPolicy(timeout, delayFunc);
+            await facade.ReleaseAsync(
+                    distributedLock,
+                    policy,
+                    cancellationToken)
+                .ConfigureAwait(false);
+        }
+
         private class NoRetriesPolicy : IRetryPolicy
         {
             public static readonly NoRetriesPolicy Instance = new(); 
@@ -71,6 +199,30 @@ namespace DistributedLocking.Extensions.Abstractions.Facades
             public bool CanRetry(RetryExecutionMetadata metadata) => false;
 
             public TimeSpan GetDelayBeforeNextRetry(RetryExecutionMetadata metadata) => TimeSpan.Zero;
+        }
+        
+        private class TimeoutRetryPolicy : IRetryPolicy
+        {
+            private readonly TimeSpan _timeout;
+            private readonly Func<RetryExecutionMetadata, TimeSpan> _delayFunc;
+
+            public TimeoutRetryPolicy(TimeSpan timeout, TimeSpan delay) : this(timeout, _ => delay)
+            {
+            }
+
+            public TimeoutRetryPolicy(TimeSpan timeout, Func<RetryExecutionMetadata, TimeSpan> delayFunc)
+            {
+                if (timeout < TimeSpan.Zero)
+                {
+                    throw new ArgumentException("Retry timeout cannot be lower than zero.");
+                }
+                _timeout = timeout;
+                _delayFunc = delayFunc ?? throw new ArgumentNullException(nameof(delayFunc));
+            }
+
+            public bool CanRetry(RetryExecutionMetadata metadata) => metadata.Elapsed < _timeout;
+
+            public TimeSpan GetDelayBeforeNextRetry(RetryExecutionMetadata metadata) => _delayFunc(metadata);
         }
     }
 }
