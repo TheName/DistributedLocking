c99f9400585fc6290b9e4d22fd193d2fb97e8954
Dawid Szmidka <dawid.szmidka@gmail.com>
2021-02-16 22:24:37 +0100

    Add DbUp extensions for IDistributedLocksRepositoryMigrationsProvider

---
+++ Repositories/Migrations/Repositories.Migrations.DbUp/DistributedLockingScriptProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DbUp.Engine;
+using DbUp.Engine.Transactions;
+using DistributedLocking.Abstractions.Repositories.Migrations;
+
+namespace DistributedLocking.Repositories.Migrations.DbUp
+{
+    /// <summary>
+    /// DbUp script provider for DistributedLocking package
+    /// </summary>
+    public class DistributedLockingScriptProvider : IScriptProvider
+    {
+        private readonly IDistributedLocksRepositoryMigrationsProvider _migrationsProvider;
+
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        /// <param name="migrationsProvider">
+        /// The <see cref="IDistributedLocksRepositoryMigrationsProvider"/>.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="migrationsProvider"/> is null.
+        /// </exception>
+        public DistributedLockingScriptProvider(IDistributedLocksRepositoryMigrationsProvider migrationsProvider)
+        {
+            _migrationsProvider = migrationsProvider ?? throw new ArgumentNullException(nameof(migrationsProvider));
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<SqlScript> GetScripts(IConnectionManager connectionManager)
+        {
+            var migrationScripts = _migrationsProvider.GetMigrationsAsync().GetAwaiter().GetResult();
+
+            return migrationScripts
+                .Select(script => new SqlScript(script.Name, script.Content));
+        }
+    }
+}
---
+++ Repositories/Migrations/Repositories.Migrations.DbUp/Extensions/UpgradeEngineBuilderExtensions.cs
@@ -0,0 +1,44 @@
+﻿using DbUp.Builder;
+using DistributedLocking.Abstractions.Repositories.Migrations;
+
+namespace DistributedLocking.Repositories.Migrations.DbUp.Extensions
+{
+    /// <summary>
+    /// Extensions for <see cref="UpgradeEngineBuilder"/>
+    /// </summary>
+    public static class UpgradeEngineBuilderExtensions
+    {
+        /// <summary>
+        /// Adds distributed locking script provider with provided migrations provider type to the builder.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="UpgradeEngineBuilder"/>.
+        /// </param>
+        /// <typeparam name="TMigrationsProvider">
+        /// The <see cref="IDistributedLocksRepositoryMigrationsProvider"/> type that should be used as migrations source.
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="UpgradeEngineBuilder"/>.
+        /// </returns>
+        public static UpgradeEngineBuilder WithDistributedLockingMigrations<TMigrationsProvider>(this UpgradeEngineBuilder builder)
+            where TMigrationsProvider : IDistributedLocksRepositoryMigrationsProvider, new() =>
+            builder.WithDistributedLockingMigrations(new TMigrationsProvider());
+
+        /// <summary>
+        /// Adds distributed locking script provider with provided migrations provider to the builder.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="UpgradeEngineBuilder"/>.
+        /// </param>
+        /// <param name="migrationsProvider">
+        /// The <see cref="IDistributedLocksRepositoryMigrationsProvider"/>.
+        /// </param>
+        /// <returns>
+        /// The <see cref="UpgradeEngineBuilder"/>.
+        /// </returns>
+        public static UpgradeEngineBuilder WithDistributedLockingMigrations(
+            this UpgradeEngineBuilder builder,
+            IDistributedLocksRepositoryMigrationsProvider migrationsProvider) =>
+            builder.WithScripts(new DistributedLockingScriptProvider(migrationsProvider));
+    }
+}
