2a0b0787796033e589d595e96779581c788c7f55
Dawid Szmidka <dawid.szmidka@gmail.com>
2021-02-16 22:29:26 +0100

    Add SqlServer DbUp extensions

---
+++ Repositories/SqlServer/Extensions/Extensions.SqlServer.Migrations.DbUp/UpgradeEngineBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using DbUp.Builder;
+using DistributedLocking.Repositories.Migrations.DbUp.Extensions;
+
+namespace DistributedLocking.Repositories.SqlServer.Migrations.DbUp
+{
+    /// <summary>
+    /// Extensions for <see cref="UpgradeEngineBuilder"/>
+    /// </summary>
+    public static class UpgradeEngineBuilderExtensions
+    {
+        /// <summary>
+        /// Adds distributed locking SQL Server migrations to the builder.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="UpgradeEngineBuilder"/>.
+        /// </param>
+        /// <returns>
+        /// The <see cref="UpgradeEngineBuilder"/>.
+        /// </returns>
+        public static UpgradeEngineBuilder WithDistributedLockingSqlServerMigrations(this UpgradeEngineBuilder builder) => 
+            builder.WithDistributedLockingMigrations<SqlDistributedLocksRepositoryMigrationsProvider>();
+    }
+}
