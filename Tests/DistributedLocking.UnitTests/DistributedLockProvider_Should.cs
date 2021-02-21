40bd6f9bbf260c5a93dccf1d0aaa9f6d8c3f6c6f
Dawid Szmidka <dawid.szmidka@gmail.com>
2021-02-21 22:52:57 +0100

    add DistributedLockProvider unit tests

---
+++ Tests/DistributedLocking.UnitTests/DistributedLockProvider_Should.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using DistributedLocking.Abstractions;
+using DistributedLocking.Abstractions.Repositories;
+using Moq;
+using TestHelpers.Attributes;
+using Xunit;
+
+namespace DistributedLocking.UnitTests
+{
+    public class DistributedLockProvider_Should
+    {
+        [Fact]
+        public void Throw_When_CreatingWithNullRepository()
+        {
+            Assert.Throws<ArgumentNullException>(() => new DistributedLockProvider(null));
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void NotThrow_When_CreatingWithNotNullRepository(IDistributedLocksRepository repository)
+        {
+            _ = new DistributedLockProvider(repository);
+        }
+        
+        [Theory]
+        [AutoMoqData]
+        public async Task Throw_When_TryingToAcquire_WithNullResourceId(
+            DistributedLockTimeToLive timeToLive,
+            DistributedLockProvider provider)
+        {
+            await Assert.ThrowsAsync<ArgumentNullException>(() => provider.TryAcquireAsync(
+                null,
+                timeToLive,
+                CancellationToken.None));
+        }
+        
+        [Theory]
+        [AutoMoqData]
+        public async Task Throw_When_TryingToAcquire_WithNullTimeToLive(
+            DistributedLockResourceId resourceId,
+            DistributedLockProvider provider)
+        {
+            await Assert.ThrowsAsync<ArgumentNullException>(() => provider.TryAcquireAsync(
+                resourceId,
+                null,
+                CancellationToken.None));
+        }
+        
+        [Theory]
+        [AutoMoqData]
+        public async Task NotThrow_When_TryingToAcquire_WithoutNulls(
+            DistributedLockResourceId resourceId,
+            DistributedLockTimeToLive timeToLive,
+            DistributedLockProvider provider)
+        {
+            await provider.TryAcquireAsync(
+                resourceId,
+                timeToLive,
+                CancellationToken.None);
+        }
+        
+        [Theory]
+        [AutoMoqData]
+        public async Task ReturnDistributedLock_When_TryingToAcquire_And_RepositorySucceeds(
+            DistributedLockResourceId resourceId,
+            DistributedLockTimeToLive timeToLive,
+            [Frozen] Mock<IDistributedLocksRepository> repositoryMock,
+            DistributedLockProvider provider)
+        {
+            repositoryMock
+                .Setup(repository => repository.TryInsert(
+                    resourceId,
+                    It.IsAny<DistributedLockId>(),
+                    timeToLive,
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+            
+            var result = await provider.TryAcquireAsync(
+                resourceId,
+                timeToLive,
+                CancellationToken.None);
+            
+            Assert.NotNull(result);
+        }
+        
+        [Theory]
+        [AutoMoqData]
+        public async Task ReturnNull_When_TryingToAcquire_And_RepositoryFails(
+            DistributedLockResourceId resourceId,
+            DistributedLockTimeToLive timeToLive,
+            [Frozen] Mock<IDistributedLocksRepository> repositoryMock,
+            DistributedLockProvider provider)
+        {
+            repositoryMock
+                .Setup(repository => repository.TryInsert(
+                    resourceId,
+                    It.IsAny<DistributedLockId>(),
+                    timeToLive,
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false);
+            
+            var result = await provider.TryAcquireAsync(
+                resourceId,
+                timeToLive,
+                CancellationToken.None);
+            
+            Assert.Null(result);
+        }
+    }
+}
